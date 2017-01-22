fcf36147ccb9a674e2b81654cedd39779b6d38fe
Thomas Sarkisian <tsarkisian@gmail.com>
2017-01-22 15:23:23 -0700

    Added: API return search and href tag to search result name

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
+import { Artist } from '../../model/artist';
 
 @Component({
   moduleId:module.id,
@@ -10,13 +11,15 @@ import { SpotifyService } from '../../services/spotify.service';
 
 export class SearchComponent  { 
 searchStr: string;
+searchRes: Artist[];
 constructor(private spotifyService:SpotifyService){
 
 }
    searchMusic() {
      console.log("f:searchMusic: ", this.searchStr);
      this.spotifyService.searchMusic(this.searchStr).subscribe(res => {
-        console.log(res.artists.items);
+        // console.log(res.artists.items);
+        this.searchRes = res.artists.items;
      })
    }
 }
