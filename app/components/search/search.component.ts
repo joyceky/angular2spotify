6aa69d5a392e4e19f52597b7496baa7e70d36019
Kyle Joyce <kyyjoyce@gmail.com>
2017-01-22 14:39:13 -0700

    update part4

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { SpotifyService } from '../../services/spotify.service'
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   moduleId: module.id,
@@ -24,28 +23,3 @@ export class SearchComponent  {
       })
   }
 }
-=======
-import { SpotifyService } from '../../services/spotify.service';
-
-@Component({
-  moduleId:module.id,
-  selector: 'search',
-  templateUrl: `search.component.html`,
-  providers: [SpotifyService]
-})
-
-export class SearchComponent  { 
-searchStr: string;
-constructor(private spotifyService:SpotifyService){
-
-}
-   searchMusic() {
-     console.log("f:searchMusic: ", this.searchStr);
-     this.spotifyService.searchMusic(this.searchStr).subscribe(res => {
-        console.log(res.artists.items);
-     })
-   }
-}
-
-
->>>>>>> origin/tps-part1
